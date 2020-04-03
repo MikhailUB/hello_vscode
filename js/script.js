aab6361f8ab738cb6b2a0e6b25e6d3463d9a331c
Mikhail Bolotov <bolotovmu@mail.ru>
2020-04-04 00:51:26 +0300

    First version

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function dynamic_adapt() {
+	console.log('sfafafa');
+
+	let w = document.querySelector('body').offsetWidth;
+	for (let index = 0; index < array.length; index++) {
+		const element = array[index];
+
+
+
+	}
+
+}
